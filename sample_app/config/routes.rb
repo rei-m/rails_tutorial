23c241b92c22da63684ecef898c407fa4cb5ed2a
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-12-26 18:04:59 +0900

    finish static pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  # この宣言でstatic_pages_about_urlヘルパーが使えるようになる
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
