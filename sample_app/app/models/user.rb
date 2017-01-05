7c0581b0faf788b0fb09c4b221680d55cdd7bcba
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2017-01-06 08:17:51 +0900

    finish chapter_12_1

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Virtual Attributesを設定
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   # callback
   before_create :create_activation_digest
   before_save   :downcase_email
@@ -40,6 +40,18 @@ class User < ApplicationRecord
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   class << self
